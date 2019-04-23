d096ed4c9dad8ac67d35c4a370d0338148739649
Lukanin Mikhail <mlukanin@ics.perm.ru>
2019-04-23 17:07:45 +0500

    Добавил начальную версию

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+  $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true
+    });
+  $('.dropdown-trigger').dropdown({
+    constrainWidth: false,
+    container: $('.yui')
+  });
+});

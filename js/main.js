7a5784378b0efdbd6d048da0e3bbb685ed1d3073
Lukanin Mikhail <mlukanin@ics.perm.ru>
2019-09-11 14:44:40 +0500

    Signed-off-by: Lukanin Mikhail <mlukanin@ics.perm.ru>

--- js/main.js
+++ js/main.js
@@ -8,7 +8,31 @@ $(document).ready(function(){
     constrainWidth: false,
     container: $('.yui')
   });
+  $('.modal').modal();
+  $('select').formSelect();
+  $('textarea#textarea1').characterCounter();
+  $('div.slider').slider();
+  $('.arrow-right').click(function(){
+    $('div.slider').slider('next');
+  });
+  $('.arrow-left').click(function(){
+    $('div.slider').slider('prev');
+  });
 });
 window.onload = function (){
   $('#loaders').hide();
+}
+function openModal(trigger = 0){
+  switch(trigger) {
+  case 0:  // if (x === 'value1')
+    //$('#modalMainContent').html("HELLO");
+    break;
+
+  case 1:  // if (x === 'value2')
+    break;
+
+  default:
+    break;
+  }
+  $('.modal').modal('open');
 }
